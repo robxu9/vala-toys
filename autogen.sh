6fceeccde8aa5dff7f0bb91cc23ff73ce8502945
Andrea Del Signore <sejerpz@tin.it>
2010-08-24 10:43:32 +0000

    2010-08-24  Andrea Del Signore  <sejerpz@tin.it>
    	* autogen.sh:
    
    	check for valac presence

--- autogen.sh
+++ autogen.sh
@@ -6,4 +6,14 @@ test -z "$srcdir" && srcdir=.
 
 PKG_NAME="vtg"
 
+test -z "$VALAC" && VALAC=valac
+if ! $VALAC --version | sed -e 's/^.*\([0-9]\+\.[0-9]\+\.[0-9]\+\).*$/\1/' | grep -vq '^0\.[0-9]\.[0-4]'
+then
+    echo "**Error**: You must have valac >= 0.9.5 installed"
+    echo "  to build Vala Toys. Download the appropriate package"
+    echo "  from your distribution or get the source tarball at"
+    echo "  http://download.gnome.org/sources/vala/"
+    exit 1
+fi
+
 . gnome-autogen.sh
